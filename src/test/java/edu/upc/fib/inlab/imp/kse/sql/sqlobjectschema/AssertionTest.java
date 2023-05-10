4a2e3518d881dcb429b2ac499dcddfa488b12df2
marti.juanola <marti.juanola@upc.edu>
2023-05-10 16:42:52 +0200

    Assertion test.

---
+++ src/test/java/edu/upc/fib/inlab/imp/kse/sql/sqlobjectschema/AssertionTest.java
@@ -0,0 +1,43 @@
+package edu.upc.fib.inlab.imp.kse.sql.sqlobjectschema;
+
+import edu.upc.fib.inlab.imp.kse.sql.services.parser.SQLObjectSchemaParser;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class AssertionTest {
+
+    @Test
+    public void shouldGenerateViewFromSimpleAssertion() {
+        String schema = """
+            CREATE TABLE LINEITEM (L_ORDERKEY int, L_COMMITDATE int);
+            CREATE TABLE ORDERS (O_ORDERKEY int, O_ORDERDATE int);
+            """;
+
+        String assertionString = """
+            CREATE ASSERTION correctDates CHECK ( NOT EXISTS (
+                SELECT *
+                FROM LINEITEM AS l JOIN ORDERS AS o ON (l.L_ORDERKEY = o.O_ORDERKEY)
+                WHERE l.L_COMMITDATE < o.O_ORDERDATE
+            ));
+            """;
+
+        String viewString = """
+            CREATE VIEW correctDates AS SELECT *
+                FROM LINEITEM AS l JOIN ORDERS AS o ON (l.L_ORDERKEY = o.O_ORDERKEY)
+                WHERE l.L_COMMITDATE < o.O_ORDERDATE
+            ;
+            """;
+
+        SQLObjectSchemaParser parser = new SQLObjectSchemaParser();
+        parser.parse(schema);
+        parser.parse(assertionString);
+        parser.parse(viewString);
+
+        Assertion assertion = parser.getSQLObjectSchema().getAssertions().get(0);
+        View expectedView = parser.getSQLObjectSchema().getViews().get(0);
+
+        assertThat("Obtained view is not equal to expected views.",
+            assertion.getEquivalentViolationDetectionView().equals(expectedView));
+    }
+}
